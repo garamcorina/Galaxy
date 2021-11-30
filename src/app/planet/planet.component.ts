9b904a81b1728362f078830c9d198e0f31c0fd8b
garamcorina <corina.garam@lola.tech>
2021-11-30 17:28:37 +0200

    separate components

--- src/app/planet/planet.component.ts
+++ src/app/planet/planet.component.ts
@@ -1,23 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Planets } from '../planets';
-import { PlanetsService } from '../planets.service';
+import { Component, Input, OnInit } from "@angular/core";
+import { Planets } from "../planets";
+import { PlanetsService } from "../planets.service";
 
 @Component({
-  selector: 'app-planet',
-  templateUrl: './planet.component.html',
-  styleUrls: ['./planet.component.css'],
+  selector: "app-planet",
+  templateUrl: "./planet.component.html",
+  styleUrls: ["./planet.component.css"],
 })
 export class PlanetComponent implements OnInit {
   @Input() postsData: Planets[] = [];
-  color: string = this.service.getPlanetColor();
+  @Input() planetColor!: string;
 
   constructor(private service: PlanetsService) {}
-  
-  ngOnChanges() {
-  }
 
-  ngOnInit(): void {
+  ngOnChanges() {}
 
-  }
-
+  ngOnInit(): void {}
 }
