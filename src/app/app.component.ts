ce3d22f891642199f75689edcff1c2f1fa2f6a4d
garamcorina <corina.garam@lola.tech>
2021-12-01 11:49:14 +0200

    improve scrolling effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { Planets } from './planets';
-import { PlanetsService } from './planets.service';
+import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
+import { Planets } from "./planets";
+import { PlanetsService } from "./planets.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   displayedData: Planets[] = [];
@@ -18,50 +18,44 @@ export class AppComponent {
   constructor(private http: HttpClient, private service: PlanetsService) {}
 
   ngOnInit() {
-    this.getPlanets(`https://swapi.py4e.com/api/planets/?page=1`);
+    this.getPlanets(`https://swapi.py4e.com/api/planets/`);
   }
 
   getPlanets(url: string): void {
     this.http.get(url).subscribe((data: any) => {
       this.displayedData = this.service.filterDiameters(data.results);
+      this.service.calculateDiameters(this.displayedData);
       this.displayedData.forEach((element: any) => {
         this.appendItems(element);
       });
       this.nextPage = data.next;
       this.previousPage = data.previous;
-      this.service.calculateDiameters(this.fetchedData);
     });
   }
 
   onScrollDown() {
-    if (this.nextPage === null) {
-      this.getPlanets(this.previousPage);
-    } else {
+    if (this.nextPage !== null) {
       this.getPlanets(this.nextPage);
-    }
+    } else return;
   }
 
   onScrollUp() {
-    if (this.previousPage === null) {
-      this.getPlanets(this.nextPage);
-    } else {
-      this.getPlanets(this.previousPage);
-    }
+    // this.getPlanets(this.previousPage);
   }
 
   appendItems(element: any) {
-    this.addItems('push', element);
+    this.fetchedData.push(element);
   }
 
   prependItems(element: any) {
-    this.addItems('unshift', element);
+    this.fetchedData.unshift(element);
   }
 
-  addItems(_method: string, element: any) {
-    if (_method === 'push') {
-      this.fetchedData.push(element);
-    } else if (_method === 'unshift') {
-      this.fetchedData.unshift(element);
-    }
-  }
+  // addItems(_method: string, element: any) {
+  //   if (_method === "push") {
+  //     this.fetchedData.push(element);
+  //   } else if (_method === "unshift") {
+  //     this.fetchedData.unshift(element);
+  //   }
+  // }
 }
